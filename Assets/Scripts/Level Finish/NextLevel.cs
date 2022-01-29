5ebb144e2fa4d0b2754955e3302674d1db145ef0
Jonne <jonnerijkhof@gmail.com>
2022-01-29 15:41:36 +0100

    sends the player to the level end screen when finished

--- Assets/Scripts/Level Finish/NextLevel.cs
+++ Assets/Scripts/Level Finish/NextLevel.cs
@@ -1,25 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour
 {
     [SerializeField] Finish _p1;
     [SerializeField] Finish _p2;
-    [SerializeField] GameObject winScreen;
-    // Start is called before the first frame update
-    void Start()
-    {
-        winScreen.SetActive(false);
-    }
-
-    // Update is called once per frame
     void Update()
     {
         if (_p1._playerHasFinished && _p2._playerHasFinished)
         {
-            Debug.Log("Level Complete");
-            winScreen.SetActive(true);
+            SceneManager.LoadScene("LevelEndMenuScene");
         }
     }
 }

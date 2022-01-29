523c26d9e8b0d4ce74c1b492eb371402a335fce6
hoodiefabio <73169440+hoodiefabio@users.noreply.github.com>
2022-01-29 19:17:24 +0100

    Horizontal transport functional

--- Assets/Scripts/Objects/Magnet Boxes/BoxPhysics.cs
+++ Assets/Scripts/Objects/Magnet Boxes/BoxPhysics.cs
@@ -7,6 +7,7 @@ public class BoxPhysics : MonoBehaviour
     public bool _isBig;
     public bool _isPusched;
     public bool _isPulled;
+    public bool _onRails;
     [SerializeField] AudioSource boxHit;
 
     private Rigidbody2D rb;
@@ -30,15 +31,15 @@ public class BoxPhysics : MonoBehaviour
     void Update()
     {
         //if the full box is lower then 0, gravity is downwards
-        if (transform.position.y < -transform.lossyScale.y)
+        if (transform.position.y < -(transform.lossyScale.y/2))
             normalGravity = 1f;
 
         //if the full box is higher then 0, gravity is upwards
-        if (transform.position.y > +transform.lossyScale.y)
+        if (transform.position.y > +(transform.lossyScale.y/2))
             normalGravity = -1f;
 
         //Box doesn't have gravity when being pushed or pulled
-        if (_isPulled || _isPusched)
+        if (_isPulled || _isPusched || _onRails)
         {
             rb.gravityScale = 0;
         }

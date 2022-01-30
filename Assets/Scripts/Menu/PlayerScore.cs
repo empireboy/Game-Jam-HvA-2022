c4a918da32193db6ee92ef2b041c9833c4ab6f1e
Jonne <jonnerijkhof@gmail.com>
2022-01-30 11:05:17 +0100

    shows the time in a time format (00:00:00)
    looks kinda weird under 10 seconds.
    why am i still seeing this fking warning screen when im going to the main menu!???

--- Assets/Scripts/Menu/PlayerScore.cs
+++ Assets/Scripts/Menu/PlayerScore.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -16,14 +17,33 @@ public class PlayerScore : MonoBehaviour
 
         if(time > 3600)//if someone has managed to play for more than an hour
         {
-
+            int hours = TimeSpan.FromSeconds(time).Hours;
+            int minutes = TimeSpan.FromSeconds(time).Minutes;
+            int seconds = TimeSpan.FromSeconds(time).Seconds;
+            timeText.text = "Your time: " + hours + ":" + minutes+ ":" + seconds;
         }
         else
         {
-
+            int minutes = TimeSpan.FromSeconds(time).Minutes;
+            int seconds = TimeSpan.FromSeconds(time).Seconds;
+            timeText.text = "Your time: " + minutes + ":" + seconds;
         }
-        timeText.text = "Your time: " + ((int)PlayerTimer._timeSpentInLevel).ToString() + " seconds";
-        fastestTimeText.text = "Your fastest time: " + PlayerPrefs.GetInt("FastestTimeLevel" + PlayerPrefs.GetInt("LastCompletedLevel")).ToString() + " seconds";
+
+        if (PlayerPrefs.GetInt("FastestTimeLevel" + PlayerPrefs.GetInt("LastCompletedLevel")) > 3600)
+        {
+            int hours = TimeSpan.FromSeconds(PlayerPrefs.GetInt("FastestTimeLevel" + PlayerPrefs.GetInt("LastCompletedLevel"))).Hours;
+            int minutes = TimeSpan.FromSeconds(PlayerPrefs.GetInt("FastestTimeLevel" + PlayerPrefs.GetInt("LastCompletedLevel"))).Minutes;
+            int seconds = TimeSpan.FromSeconds(PlayerPrefs.GetInt("FastestTimeLevel" + PlayerPrefs.GetInt("LastCompletedLevel"))).Seconds;
+            fastestTimeText.text = "Your time: " + hours + ":" + minutes + ":" + seconds;
+        }
+        else
+        {
+            int minutes = TimeSpan.FromSeconds(PlayerPrefs.GetInt("FastestTimeLevel" + PlayerPrefs.GetInt("LastCompletedLevel"))).Minutes;
+            int seconds = TimeSpan.FromSeconds(PlayerPrefs.GetInt("FastestTimeLevel" + PlayerPrefs.GetInt("LastCompletedLevel"))).Seconds;
+            fastestTimeText.text = "Your fastest time: " + minutes + ":" + seconds;
+        }
+        //timeText.text = "Your time: " + ((int)PlayerTimer._timeSpentInLevel).ToString() + " seconds";
+        //fastestTimeText.text = "Your fastest time: " + PlayerPrefs.GetInt("FastestTimeLevel" + PlayerPrefs.GetInt("LastCompletedLevel")).ToString() + " seconds";
 
         switch (PlayerTimer.starsEarned)
         {

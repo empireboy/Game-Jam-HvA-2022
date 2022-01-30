d68e9d66147a81868b155b9dfacd1265d6bf1328
Jonne <jonnerijkhof@gmail.com>
2022-01-30 16:18:40 +0100

    this should fix it

--- Assets/Scripts/Menu/AmountOfLevels.cs
+++ Assets/Scripts/Menu/AmountOfLevels.cs
@@ -8,8 +8,8 @@ public class AmountOfLevels : MonoBehaviour
     void Start()
     {
         //run at the start of the entire game
-        PlayerPrefs.SetInt("PlayingLevel", 1);
-        PlayerPrefs.SetInt("LevelUnlocked", 1);
+        if (PlayerPrefs.GetInt("PlayingLevel") == 0) PlayerPrefs.SetInt("PlayingLevel", 1);
+        if (PlayerPrefs.GetInt("LevelUnlocked") == 0) PlayerPrefs.SetInt("LevelUnlocked", 1);
         PlayerPrefs.SetInt("AmountOfLevels", amountOfLevels);
     }
 }
